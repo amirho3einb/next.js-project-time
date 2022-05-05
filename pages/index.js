8c2c9ebb8b7af9662bb5fe664465f358a5234d38
Amirhossein Balagar <amirhossinbalagar@gmail.com>
2022-05-05 14:53:02 +0430

    Adding Regular React Components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import { getFeaturedEvents } from "../dummy-data";
+
 const HomePage = () => {
-  return <div>The Home Page</div>;
+  const featuredEvents = getFeaturedEvents();
+  return <div>
+    <ul>
+      {featuredEvents.map((event)=> <li> </li>)}
+    </ul>
+  </div>;
 };
 
 export default HomePage;
